2c86c3474dc42bbaacbe80e1ea3638397fc75573
muslimkhan44 <102409260+muslimkhan44@users.noreply.github.com>
2022-04-02 01:43:59 -0700

    Add files via upload

---
+++ swanky_curcan1.ino
@@ -0,0 +1,43 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+ 
+}
+
+void loop()
+{
+  digitalWrite(0,HIGH);
+  delay(800); // Wait for 1000 millisecond(s)
+  digitalWrite(0, LOW);
+  delay(800);
+ digitalWrite(1,HIGH);
+  delay(800); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+    delay(800);
+  digitalWrite(2,HIGH);
+  delay(800); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+    delay(800);
+  digitalWrite(3,HIGH);
+  delay(800); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+    delay(800);
+  digitalWrite(4,HIGH);
+  delay(800); // Wait for 1000 millisecond(s)
+  digitalWrite(4,LOW);
+    delay(800);
+ digitalWrite(5,HIGH);
+  delay(800); // Wait for 1000 millisecond(s)
+  digitalWrite(5, LOW);
+    delay(800);
+ 
+ 
+ 
+ 
+}
